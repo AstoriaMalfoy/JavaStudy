e52288fa2348515b124047e8c44348af041c1628
ASTORIA <2765036345@qq.com>
2023-02-10 06:08:28 +0000

    add/

---
+++ springboot/src/main/java/com/astocoding/App.java
@@ -0,0 +1,12 @@
+package com.astocoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+        System.out.println("the spring boot test applicaiton start success");
+    }
+}
